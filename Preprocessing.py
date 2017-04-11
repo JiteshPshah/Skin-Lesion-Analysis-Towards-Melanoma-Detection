6530bbcabb5442326407f4dcfa251984294ebb5b
Jitesh Shah <jitesh2k12@gmail.com>
2017-04-11 15:28:45 +0530

    Add files via upload

--- Preprocessing.py
+++ Preprocessing.py
@@ -6,21 +6,16 @@ Created on Thu Mar 16 16:28:21 2017
 @author: edward
 """
 
-
 import cv2
 import os
+from PIL import Image
 def load_images(folder):
         image_list=[]
         for filename in sorted(os.listdir(folder)):
             img=cv2.imread(os.path.join(folder,filename))
             image_list.append(img)
         return image_list
-def resize_images(images):
-    resize_image=[]
-    for image in images:
-        imag=cv2.resize(image,(64,64))
-        resize_image.append(imag)
-    return resize_image
+        print "Images are loaded"
 
 def RemoveBackground(Image):
     IGray=cv2.cvtColor(Image,cv2.COLOR_BGR2GRAY)
@@ -30,7 +25,16 @@ def RemoveBackground(Image):
     maskedImage=cv2.bitwise_and(Image,Image,mask=thresh2)
     return maskedImage
 
-def save_images(Image):
-    for i in range(1,len(Image)+1):
-        cv2.imwrite(('imageresized_{}.jpg'.format(i)),Image[i-1])
-        
+def Resize_Images(path,path2):
+    for x in os.listdir(path):
+        base = x
+        x=path + '/' + x
+    
+    	if os.path.isdir(x) == True:
+		print "Skipping directory " + x
+		continue
+        print x
+	im1 = Image.open(x)
+	im2 = im1.resize((64, 64))    
+	im2.save(path2+"/"+base)
+    print "Images are stored at {}".format(path2) 
